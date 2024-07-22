0462cf95fbb75d0c6b5ccef0c8130109f2caa5f8
ericguo-foregrounds <eric@foregrounds.ai>
2024-07-22 18:36:40 -0400

    Update index.js
    Accounts for pagination on Insidr.ai

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const { chromium } = require('playwright');  // Or 'firefox' or 'webkit'.
 
 (async () => {
-  const browser = await chromium.launch();
-  const page = await browser.newPage();
-  await page.goto('https://www.insidr.ai/ai-tools/');
-  // other actions...
-  await page.screenshot( {path: "page2.png", fullPage: true});
-  await browser.close();
+    const browser = await chromium.launch(); // launches Chromium
+    const page = await browser.newPage();
+
+    const numPages = 4; // number of pages we want to scrape
+    const baseUrl = "https://www.insidr.ai/ai-tools/page/" // will be used in for loop
+
+    for(let pageNum = 1; pageNum <= numPages; pageNum++) { // solves pagination issue
+        const url = `${baseUrl}${pageNum}/`;
+
+        try {
+            await page.goto(url);
+            await page.screenshot({ path: `page${pageNum}.png`, fullPage: true });
+        }
+        catch { // if error
+            console.log(`Error on Page ${pageNum}`); // tells us where error occurred
+            await browser.close();
+        }
+    }
+    await browser.close();
 })();
 
 
